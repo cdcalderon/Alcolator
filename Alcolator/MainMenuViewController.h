cdd06275525e73520c5aa19ba222cac383c3fb1b
carlos calderon <cdcalderon@gmail.com>
2015-10-18 15:25:58 -0500

    assignment 21 complete

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Carlos Calderon on 10/18/15.
+//  Copyright (c) 2015 Carlos Calderon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
