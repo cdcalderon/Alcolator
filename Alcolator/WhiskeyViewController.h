9e378f3cb8be955440717091a9b322deea4c1c0d
carlos calderon <cdcalderon@gmail.com>
2015-10-18 02:03:10 -0500

    assignment 20 complete

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Carlos Calderon on 10/18/15.
+//  Copyright (c) 2015 Carlos Calderon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
